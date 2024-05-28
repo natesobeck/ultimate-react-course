c509078401b491b632ae773f54063533d4b84abb
Nate Sobeck <nsobeck910@gmail.com>
2024-05-28 15:05:51 -0400

    solve accordion v2 challenge

--- projects/04-steps/accordion/src/Accordion.js
+++ projects/04-steps/accordion/src/Accordion.js
@@ -1,11 +1,26 @@
 import { faqs } from "./App"
 import AccordionItem from "./AccordionItem"
+import { useState } from "react"
 
 const Accordion = () => {
+  const [currentOpen, setCurrentOpen] = useState(null)
+  
+  function handleClick(num) {
+    setCurrentOpen(num)
+  }
+
   return (
     <div className="accordion">
       {faqs.map((faq, i) => (
-        <AccordionItem key={faq.title} num={i + 1} title={faq.title} text={faq.text}/>
+        <AccordionItem
+          currentOpen={currentOpen}
+          onCurrentOpen={setCurrentOpen}
+          key={faq.title}
+          num={i + 1}
+          title={faq.title}
+          text={faq.text}
+          onClick={handleClick}
+        />
       ))}
     </div>
   )
--- projects/04-steps/accordion/src/AccordionItem.js
+++ projects/04-steps/accordion/src/AccordionItem.js
@@ -1,10 +1,12 @@
-import { useState } from "react"
+const AccordionItem = ({ num, title, text, currentOpen, onClick }) => {
 
-const AccordionItem = ({ num, title, text }) => {
-  const [isOpen, setIsOpen] = useState(false)
+  const isOpen = num === currentOpen
 
   return (
-    <div className={`item ${isOpen ? "open" : ""}`} onClick={() => setIsOpen(!isOpen)}>
+    <div
+      className={`item ${isOpen ? "open" : ""}`}
+      onClick={() => onClick(num)}
+    > 
       <p className="number">{num < 9 ? `0${num}` : num}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "+" : "-"}</p>
