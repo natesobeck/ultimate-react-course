0e4f373e8b8da4348db6614b5c58e8c0d3bf7c4e
Nate Sobeck <nsobeck910@gmail.com>
2024-06-20 13:40:27 -0400

    finish currency converter challenge

--- projects/mini-projects/part-2-intermediate-react/currency-converter/src/App.js
+++ projects/mini-projects/part-2-intermediate-react/currency-converter/src/App.js
@@ -1,31 +1,67 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 function App() {
-  const [currencyInput, setCurrencyInput] = useState("")
-  const [state, setState] = useState('State test')
+  const [amount, setAmount] = useState("")
+  const [fromCurrency, setFromCurrency] = useState("USD")
+  const [toCurrency, setToCurrency] = useState("EUR")
+  const [output, setOutput] = useState("Add a value to see conversion")
 
-  const handleChange = (e) => {
-    setCurrencyInput(e.target.value)
+  const handleCurrencyInput = (e) => {
+    const numInputValue = Number(e.target.value)
+
+    setAmount(
+      isNaN(numInputValue) ? amount : numInputValue === 0 ? "" : numInputValue
+    )
   }
+
+  const handleCurrencySelect = (e) => {
+    if (e.target.classList.contains("from-currency")) {
+      setFromCurrency(e.target.value)
+    }
+    if (e.target.classList.contains("to-currency")) {
+      setToCurrency(e.target.value)
+    }
+  }
+
+  useEffect(() => {
+    async function convertCurrency() {
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`
+      )
+      const data = await res.json()
+      setOutput(data.rates[toCurrency])
+    }
+    if (amount !== '') {
+      convertCurrency()
+    }
+  }, [amount, fromCurrency, toCurrency])
 
   return (
     <div>
-      <input type="text" onChange={handleChange} value={currencyInput}/>
-      <select>
+      <input type="text" onChange={handleCurrencyInput} value={amount} />
+      <select
+        value={fromCurrency}
+        onChange={(e) => handleCurrencySelect(e)}
+        className="from-currency"
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-        <option value="CAD">CAD</option>
-        <option value="INR">INR</option>
+      <select
+        value={toCurrency}
+        className="to-currency"
+        onChange={(e) => handleCurrencySelect(e)}
+      >
+        {fromCurrency !== "USD" && <option value="USD">USD</option>}
+        {fromCurrency !== "EUR" && <option value="EUR">EUR</option>}
+        {fromCurrency !== "CAD" && <option value="CAD">CAD</option>}
+        {fromCurrency !== "INR" && <option value="INR">INR</option>}
       </select>
-      <p>OUTPUT</p>
+      <p>{amount} in {fromCurrency} is {output} in {toCurrency}</p>
     </div>
   )
 }
