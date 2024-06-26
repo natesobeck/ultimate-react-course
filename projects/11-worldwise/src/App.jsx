36073c9fbb49cc39c38de698b538cc6193e977fb
Nate Sobeck <nsobeck910@gmail.com>
2024-06-26 11:48:10 -0400

    begin worldwise project

--- projects/11-worldwise/src/App.jsx
+++ projects/11-worldwise/src/App.jsx
@@ -1,10 +1,23 @@
-function App() {
-  const x = 23
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Homepage from "./pages/Homepage"
+import Pricing from "./pages/Pricing"
+import Product from "./pages/Product"
+import PageNotFound from "./pages/PageNotFound"
+import AppLayout from "./pages/AppLayout"
+import Login from "./pages/Login"
 
+function App() {
   return (
-    <div>
-      sanity check
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage />}/>
+        <Route path='pricing' element={<Pricing />}/>
+        <Route path='product' element={<Product />}/>
+        <Route path='login' element={<Login />}/>
+        <Route path='app' element={<AppLayout />}/>
+        <Route path='*' element={<PageNotFound />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ projects/11-worldwise/src/pages/AppLayout.jsx
@@ -0,0 +1,13 @@
+import PageNav from "../components/PageNav/PageNav"
+import AppNav from "../components/AppNav/AppNav"
+
+const AppLayout = () => {
+  return (
+    <div>
+      <PageNav /> APPPPPP
+      <AppNav />
+    </div>
+  )
+}
+
+export default AppLayout
